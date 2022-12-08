26dc10ecb123d884540ea281232249ce80003901
Balls <nicolajkorchikov54@gmail.com>
2022-12-08 12:20:29 +0200

    finish footer and project at all

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,17 +19,37 @@ const Footer = () => (
             <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
                 {footerLinks.map((footerLink) => (
                     <div key={footerLink.key} className="flex flex-col ss:my-0 my-4 min-w-[150px]">
-                        <h4>
+                        <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
                             {footerLink.title}
                         </h4>
-                        <ul>
-                            {footerLink.links.map((link) => (
-                                <li key={link.name} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer`}>
+                        <ul className="list-none mt-4">
+                            {footerLink.links.map((link, index) => (
+                                <li key={link.name} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite 
+                                hover:text-secondary cursor-pointer 
+                                ${index !== footerLink.links.length - 1 ? 'mb-4' : 'mb-0'}`
+                                }>
                                     {link.name}
                                 </li>
                             ))}
                         </ul>
                     </div>
+                ))}
+            </div>
+        </div>
+
+        <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
+            <p className="font-poppins font-normal text-center text-[16px] leading-[27px] text-dimWhite">
+            Copyright Ⓒ 2021 HooBank. All Rights Reserved.
+            </p>
+
+            <div className="flex flex-row md:mt-0 mt-6">
+                {socialMedia.map((social, index) => (
+                  <img 
+                  key={social.id}
+                  src={social.icon}
+                  alt={social.id}
+                  className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? "mr-6" : "mr-0"}`}
+                   /> 
                 ))}
             </div>
         </div>
